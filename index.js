1d79a134209a080981f91c624669647b65108244
Tommy Lindeberg <lindeberg.tommy@gmail.com>
2019-05-22 09:18:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,5 +22,9 @@ function sayHiToGrandma(string) {
  else if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
+  
+ else (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }
  
 }
