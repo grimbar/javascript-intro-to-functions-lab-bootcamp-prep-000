d67a06bc61dcc772901a6350bcb939fb7305fab6
Tommy Lindeberg <lindeberg.tommy@gmail.com>
2019-05-22 09:00:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,23 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log("hello".toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log('HELLO'.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  return 'I can\'t hear you!'
+}
+
+function sayHiToGrandma (string) {
+  return 'YES INDEED!'
+}
